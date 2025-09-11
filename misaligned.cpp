6cc156ceeb83bc345ea818647501cefc5baefc98
ninantony25 <57633711+ninantony25@users.noreply.github.com>
2025-09-11 22:16:40 +0530

    Update misaligned.cpp

--- misaligned.cpp
+++ misaligned.cpp
@@ -3,31 +3,35 @@
 #include <string>
 #include <cassert>
  
-std::vector<std::string> getColorMap() {
+std::vector<std::string> getColorMap() 
+{
     const std::vector<std::string> major = {"White", "Red", "Black", "Yellow", "Violet"};
     const std::vector<std::string> minor = {"Blue", "Orange", "Green", "Brown", "Slate"};
     std::vector<std::string> colorMap;
     colorMap.reserve(25);
  
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            colorMap.push_back(
-                std::to_string(i * 5 + j) + " | " + major[i] + " | " + minor[i]  // BUG: minor[i] should be minor[j]
-            );
+    for (int i = 0; i < 5; ++i) 
+    {
+        for (int j = 0; j < 5; ++j) 
+        {
+            colorMap.push_back(std::to_string(i * 5 + j) + " | " + major[i] + " | " + minor[i]  // BUG: minor[i] should be minor[j]);
         }
     }
     return colorMap;
 }
  
-int printColorMap() {
+int printColorMap() 
+{
     const auto& colorMap = getColorMap();
-    for (const auto& entry : colorMap) {
+    for (const auto& entry : colorMap) 
+    {
         std::cout << entry << "\n";
     }
     return static_cast<int>(colorMap.size());
 }
  
-void testPrintColorMap() {
+void testPrintColorMap() 
+{
     std::cout << "\nPrint color map test\n";
     auto colorMap = getColorMap();
  
@@ -39,8 +43,10 @@ void testPrintColorMap() {
     std::cout << "All is well (maybe!)\n";
 }
  
-int main() {
+int main() 
+{
     testPrintColorMap();
     return 0;
 }
+
 
